79803cfc5a23ac2f65c6cc4ffde1301797a585f9
giuwo <167148948+giuwo@users.noreply.github.com>
2025-01-31 02:51:32 -0500

    Add files via upload

--- decrypter.py
+++ decrypter.py
@@ -1,22 +1,25 @@
 import os
 import pyaes
 
-## abrir o arquivo criptografado
-file_name = "teste.txt.ransomwaretroll"
-file = open(file_name, "rb")
+## Abre o arquivo criptografado
+
+file_name = 'teste.txt.ransomwaretroll'
+file = open (file_name, 'rb')
 file_data = file.read()
 file.close()
 
-## chave para descriptografia
-key = b"testeransomwares"
-aes = pyaes.AESModeOfOperationCTR(key)
-decrypt_data = aes.decrypt(file_data)
+## Chave de descriptografia
 
-## remover o arquivo criptografado
+key = b'testetestandotestado'
+aes = pyaes.AESModeOfOperationCTR(key)
+decrypt_data = aes.decrypt(file.data)
+
+## Remove o arquivo criptografado
 os.remove(file_name)
 
-## criar o arquivo descriptografado
-new_file = "teste.txt"
-new_file = open(f'{new_file}', "wb")
+## Cria um novo arquivo
+
+new_file = 'teste.txt'
+new_file = open(f'{new_file}', 'wb')
 new_file.write(decrypt_data)
 new_file.close()
